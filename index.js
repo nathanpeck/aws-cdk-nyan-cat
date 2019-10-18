57747f873d6ecf54023fd25f35d290fdc29ce0f4
Nathan Peck <peckn@amazon.com>
2019-10-18 12:23:37 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const ec2 = require('@aws-cdk/aws-ec2');
+const ecs = require('@aws-cdk/aws-ecs');
+const ecsPatterns = require('@aws-cdk/aws-ecs-patterns');
+const cdk = require('@aws-cdk/core');
+
+class GreetingStack extends cdk.Stack {
+  constructor(parent, id, props) {
+    super(parent, id, props);
+
+    // Create VPC and Fargate Cluster
+    // NOTE: Limit AZs to avoid reaching resource quotas
+    const vpc = new ec2.Vpc(this, 'MyVpc', { maxAzs: 2 });
+    const cluster = new ecs.Cluster(this, 'Cluster', { vpc });
+
+    // Instantiate Fargate Service with just cluster and image
+    const fargateService = new ecsPatterns.ApplicationLoadBalancedFargateService(this, 'nyan-service', {
+      cluster,
+      taskImageOptions: {
+        image: ecs.ContainerImage.fromRegistry('daviey/nyan-cat-web'),
+      },
+    });
+  }
+}
+
+const app = new cdk.App();
+const nyan = new GreetingStack(app, 'aws-cdk-nyan-cat');
+
+app.synth();
